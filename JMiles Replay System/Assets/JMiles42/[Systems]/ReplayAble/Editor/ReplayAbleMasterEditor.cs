d6c9e97d0a3f40df22719a1b361daadf51567c5d
Unknown <J@jmiles42.com>
2017-10-12 13:29:45 +1100

    Moved Replayable Files

--- JMiles Replay System/Assets/JMiles42/[Systems]/ReplayAble/Editor/ReplayAbleMasterEditor.cs
+++ JMiles Replay System/Assets/JMiles42/[Systems]/ReplayAble/Editor/ReplayAbleMasterEditor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace ReplayAble {
 	[CustomEditor(typeof (ReplayAbleMaster))]
 	public class ReplayAbleMasterEditor: CustomEditorBase {
-		private const string EXTENSION = "rply";
+		private const string EXTENSION = "txt";
 
 		public override void DrawGUI() {
 			if (JMilesGUILayoutEvents.Button("Save List")) {
@@ -16,7 +16,7 @@ namespace ReplayAble {
 		private void SaveData() {
 			var master = (ReplayAbleMaster) target;
 
-			var path = EditorUtility.SaveFilePanel("Save Replay", Application.dataPath, System.DateTime.Now.ToLongDateString(), EXTENSION);
+			var path = EditorUtility.SaveFilePanel("Save Replay", Application.dataPath,"Replay " +  System.DateTime.Now.ToString("yyyy-MM-dd hh-mm"), EXTENSION);
 
 			JMiles42.IO.Generic.SavingLoading.SaveGameDataFilepath(path, new KeyframeSavedData {Keyframes = master.KeyFrames});
 		}
