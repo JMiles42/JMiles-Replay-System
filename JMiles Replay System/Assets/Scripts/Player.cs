adf39592e51caa1d22e9994679d24142bf5f42d4
JMiles42 <J@jmiles42.com>
2017-12-11 10:54:06 +1100

    Added Speed to player

--- JMiles Replay System/Assets/Scripts/Player.cs
+++ JMiles Replay System/Assets/Scripts/Player.cs
@@ -2,10 +2,35 @@
 using JMiles42.UnityInterfaces;
 using UnityEngine;
 
-public class Player: JMilesBehavior, IEventListening {
-	public void OnEnable() { ReplayAble.ReplayAbleInputSystem.Jump.onKeyDown += OnKeyDown; }
+public class Player: JMilesBehavior, IEventListening
+{
+	public float speed = 0.5f;
+	public void OnEnable()
+	{
+		ReplayAble.ReplayAbleInputSystem.Jump.onKeyDown += OnKeyDown;
+		ReplayAble.ReplayAbleInputSystem.Horizontal.onKey += OnHorizontal;
+		ReplayAble.ReplayAbleInputSystem.Vertical.onKey += OnVertical;
+	}
 
-	public void OnDisable() { ReplayAble.ReplayAbleInputSystem.Jump.onKeyDown -= OnKeyDown; }
+	public void OnDisable()
+	{
+		ReplayAble.ReplayAbleInputSystem.Jump.onKeyDown -= OnKeyDown;
+		ReplayAble.ReplayAbleInputSystem.Horizontal.onKey -= OnHorizontal;
+		ReplayAble.ReplayAbleInputSystem.Vertical.onKey -= OnVertical;
+	}
 
-	private void OnKeyDown() { Position += Vector3.up; }
+	private void OnKeyDown()
+	{
+		Position += Vector3.up;
+	}
+
+	private void OnHorizontal(float val)
+	{
+		Position += Vector3.forward * val * speed;
+	}
+
+	private void OnVertical(float val)
+	{
+		Position += Vector3.right * val * speed;
+	}
 }
